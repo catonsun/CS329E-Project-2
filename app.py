c42bd79f1a537e5002823a3702f24f8522819152
Kevin Zhang <kevinzhang@utexas.edu>
2018-10-30 12:25:48 -0500

    addeed csv
    lol

--- app.py
+++ app.py
@@ -14,5 +14,23 @@ def main():
     numQuestions = input("How many questions do you want (5-30)?")
     print(numQuestions)
 
+    # print("Question:")
+    # print(qList[0])
+    # print("")
+    # print("Answer Choices:")
+    # print(qList[1])
+    # print(qList[2])
+    # print(qList[3])
+    # print(qList[4])
+    #
+    # print("")
+    # x = input()
+    #
+    # if x == qList[5]:
+    #     print("Good Job")
+    # else:
+    #     print("You Suck")
+
+
 
 main()
---
+++ test.py
@@ -0,0 +1,2 @@
+# selecting button will return answer choice
+# makeList returns a list
