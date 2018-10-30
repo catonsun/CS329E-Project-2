660d4a5126685dfb30783ea30abd488c2d81437a
grace-fung <37249107+grace-fung@users.noreply.github.com>
2018-10-30 11:52:30 -0500

    questions #
    getting the amount of questions users want

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ def makeList(file): #adds words individually into a list
     return (questions)
 
 def main():
+    numQuestions = 0
     file = open("Questions.txt", "r") #reading file
     quiz = file.read()
     qList = []
     qList = makeList(quiz)
-    print(qList)
+
+    numQuestions = input("How many questions do you want (5-30)?")
+    print(numQuestions)
+
 
 main()
